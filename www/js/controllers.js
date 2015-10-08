f81f8d4174f96601f5a1bcdfec52fae00d68b54c
Gabriel Cowley <gc1913@ic.ac.uk>
2015-10-08 22:10:36 +0100

    Added form

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,24 @@
 angular.module('scanner.controllers', [])
-    .controller('HomeController', function($scope, $rootScope, $cordovaBarcodeScanner, $ionicPlatform, scoreKeeperService, infoService) {
+    .controller('HomeController', function($scope, $rootScope, $cordovaBarcodeScanner, $ionicPlatform, scoreKeeperService, infoService, $ionicModal) {
         var vm = this;
                 
                 vm.score = scoreKeeperService.getScore();
                 vm.hasInfo = false;
+                
+                $ionicModal.fromTemplateUrl('templates/form-modal.html', {
+                                            scope: $scope,
+                                            animation: 'slide-in-up'
+                                            }).then(function(modal) {
+                vm.modal = modal;
+                });
+                
+                vm.openForm = function(){
+                vm.modal.show();
+                };
+                
+                vm.closeForm = function() {
+                vm.modal.hide();
+                }
 
         vm.scan = function(){
             $ionicPlatform.ready(function() {
@@ -15,12 +30,21 @@ angular.module('scanner.controllers', [])
                           vm.score = scoreKeeperService.incrementScore();
                           
                           var qr_str = result.text;
+                          
+                          if (qr_str === 'digi-FORM') {
+                          vm.hasInfo = false;
+                          vm.openForm();
+                          
+                          } else {
                           var infoObj = infoService.getInfo(qr_str);
                           
                           if (infoObj) {
                           vm.hasInfo = true;
                           vm.result = infoObj;
-                          } 
+                          }
+                          }
+                          
+                          
                           
                         // Success! Barcode data is here
                         /*vm.scanResults = "We got a barcode\n" +
